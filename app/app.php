c020cb9e9d1f8e9345585d86c450021979dcb6aa
Ethan <ethan.ccj@gmail.com>
2013-10-09 09:24:57 +0800

    initial setup

---
+++ app/app.php
@@ -0,0 +1,144 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
+ini_set('display_errors', 1);
+
+require_once('class.phpmailer.php');
+
+if (isset($_POST['email'])) {
+
+	$mail = new PHPMailer();
+
+	$mail->IsSMTP();
+	$mail->SMTPAuth = true;
+	$mail->Host = "smtp.elasticemail.com";
+	$mail->Port = 25;
+	$mail->Username = "ca42d852-f020-4d04-a1fa-9c93f5640431";
+	$mail->Password = "ca42d852-f020-4d04-a1fa-9c93f5640431";
+
+	$message = "Name: ".$_POST['name']."<br/>".
+			   	"Email: ".$_POST['email']."<br/>".
+			   	"Phone: ".$_POST['phone']."<br/>".
+			   	"Subject: ".$_POST['subject']."<br/>".
+			   	"Message<br/>: ".$_POST['message']."<br/>";
+
+	$mail->SetFrom('enquiry@redcross.org.sg', 'Red Cross Connect');
+	$mail->Subject = "Contact Us Form";
+	$mail->MsgHTML($message);
+
+	$mail->AddAddress('enquiry@redcross.org.sg', 'Red Cross Connect Enquiry');
+	$mail->AddAddress($_POST['email'], $_POST['name']);
+	$mail->addCC('christy.goh@mrmworldwide.com');
+	$mail->addCC('ethan.ccj@gmail.com');
+	
+	if($mail->Send()) {
+	   echo "result=0";
+	} else {
+	   echo "result=1";
+	}
+}
+
+if (isset($_GET['method']) == 'bloodstat') {
+
+	$clientid = '591439560144.apps.googleusercontent.com';
+	$secret = 'faY1qxOc3IOUH4dJ6Sw1UcSB';
+
+	$user = "redcrossconnectsg@gmail.com";
+	$pass = "=rccsg8303";
+	 
+	// Google Spreadsheet ID (You can get it from the URL when you view the spreadsheet)
+	$GSheetID = "0AmHch73Ue3_EcDRkZWxfUWM3eGpkdUpBY0FIcFZtYUE";
+	 
+	// od6 is the first worksheet in the spreadsheet
+	$worksheetID="ocz";
+	 
+	// Include the loader and Google API classes for spreadsheets
+	require_once('Zend/Loader.php');
+	Zend_Loader::loadClass('Zend_Gdata');
+	Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
+	Zend_Loader::loadClass('Zend_Gdata_Spreadsheets');
+	Zend_Loader::loadClass('Zend_Http_Client');
+	 
+	// Authenticate on Google Docs and create a Zend_Gdata_Spreadsheets object.            
+
+	$service = Zend_Gdata_Spreadsheets::AUTH_SERVICE_NAME;
+	$client = Zend_Gdata_ClientLogin::getHttpClient($user, $pass, $service);
+	$spreadsheetService = new Zend_Gdata_Spreadsheets($client);
+	
+	$query = new Zend_Gdata_Spreadsheets_ListQuery();
+	$query->setSpreadsheetKey($GSheetID);
+	$query->setWorksheetId($worksheetID);
+
+	$query->setSpreadsheetQuery( 'date='.date("n/j/Y").' and apos<>"" and bpos<>"" and opos<>"" and abpos<>""' );
+	$listFeed = $spreadsheetService->getListFeed($query);
+
+	//echo count($listFeed->entries);
+
+	$blood_a = 0;
+	$blood_b = 0;
+	$blood_o = 0;
+	$blood_ab = 0;
+
+	if (count($listFeed->entries) > 0) {
+		$rowData = $listFeed->entries[0]->getCustom();
+
+		$blood_a = $rowData[1]->getText();
+		$blood_b = $rowData[2]->getText();
+		$blood_o = $rowData[3]->getText();
+		$blood_ab = $rowData[4]->getText();
+
+	} else {
+		$query = new Zend_Gdata_Spreadsheets_ListQuery();
+		$query->setSpreadsheetKey($GSheetID);
+		$query->setWorksheetId($worksheetID);
+
+		//$query->setSpreadsheetQuery( 'apos<>"" and bpos<>"" and opos<>"" and abpos<>"" order by date desc' );
+
+		$listFeed = $spreadsheetService->getListFeed($query);
+
+		for($i = (count($listFeed->entries)-1); $i>0; $i--) {
+			$rowData = $listFeed->entries[$i]->getCustom();
+
+			if ($rowData[1]->getText()!='' && $rowData[2]->getText()!='' && $rowData[3]->getText()!='' && $rowData[4]->getText()!='') {
+
+				$blood_a = intval($rowData[1]->getText());
+				$blood_b = intval($rowData[2]->getText());
+				$blood_o = intval($rowData[3]->getText());
+				$blood_ab = intval($rowData[4]->getText());
+			}			
+		}
+	}
+
+	// set the blood levels
+	if ($blood_a <=269)
+		$blood_a = '25';
+	else if ($blood_a >= 270 && $blood_a <= 599)
+		$blood_a = '50';
+	else if ($blood_a >= 600)
+		$blood_a = '75';
+
+	if ($blood_b <=269)
+		$blood_b = '25';
+	else if ($blood_b >= 270 && $blood_b <= 599)
+		$blood_b = '50';
+	else if ($blood_b >= 600)
+		$blood_b = '75';
+
+	if ($blood_o <=44)
+		$blood_o = '25';
+	else if ($blood_o >= 45 && $blood_o <= 89)
+		$blood_o = '50';
+	else if ($blood_o >= 90)
+		$blood_o = '75';
+
+	if ($blood_ab <=399)
+		$blood_ab = '25';
+	else if ($blood_ab >= 400 && $blood_ab <= 809)
+		$blood_ab = '50';
+	else if ($blood_ab >= 810)
+		$blood_ab = '75';
+
+	echo $blood_a.'-'.$blood_a.'-'.$blood_o.'-'.$blood_ab;
+}
+
+
+?>
---
+++ js/main.js
@@ -0,0 +1,361 @@
+
+$(document).ready(function() {
+
+	// mobile menu
+	$('.menu').on('click', function(e) {
+		if ($('.top-nav ul:visible').size() > 0)
+			$('.top-nav ul').hide();
+		else {
+			$('.top-nav ul').show();
+		}
+		e.preventDefault();
+	});
+
+	// hide on click event in the menu
+	$('.top-nav ul li a').on('click', function(e) {
+		if ($('.for-mobile:visible').size() > 0)
+			$('.top-nav ul').hide();
+	});
+
+	// faq expand answer
+	$('.faqlist li label').on('click', function() {
+		if ($(this).parent().find('div:visible').size() == 0)
+			$(this).parent().find('div').show();
+		else
+			$(this).parent().find('div').hide();
+	});
+
+	// fancybox for contact us form
+	// jQuery 1.9 has removed the `$.browser` property, fancybox relies on
+	// it, so we patch it here if it's missing.
+	// This has been copied from jQuery migrate 1.1.1.
+	if ( !jQuery.browser ) {
+	  var uaMatch = function( ua ) {
+	    ua = ua.toLowerCase();
+
+	    var match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
+	      /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+	      /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+	      /(msie) ([\w.]+)/.exec( ua ) ||
+	      ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
+	      [];
+
+	    return {
+	      browser: match[ 1 ] || "",
+	      version: match[ 2 ] || "0"
+	    };
+	  };
+
+	  matched = uaMatch( navigator.userAgent );
+	  browser = {};
+
+	  if ( matched.browser ) {
+	    browser[ matched.browser ] = true;
+	    browser.version = matched.version;
+	  }
+
+	  // Chrome is Webkit, but Webkit is also Safari.
+	  if ( browser.chrome ) {
+	    browser.webkit = true;
+	  } else if ( browser.webkit ) {
+	    browser.safari = true;
+	  }
+
+	  jQuery.browser = browser;
+	};
+
+	// google map initialization
+	var directionsDisplay;
+	var directionsService = new google.maps.DirectionsService();
+	var map;
+	var click = 0;
+	var start;
+	var end;
+	var markers = new Array();
+	var infoWindow;
+	var current_marker;
+
+	var sgcenter = new google.maps.LatLng(1.378308,103.807899);
+	
+	var locations = [
+		{ lat : 1.2809352, lng : 103.8375668 },
+		{ lat : 1.4359454, lng : 103.7861284 },
+		{ lat : 1.3001956, lng : 103.8477121 }
+	];
+
+	var mapProp = {
+		center:sgcenter,
+		zoom:11,
+		panControl:true,
+		zoomControl:true,
+		mapTypeControl:true,
+		scaleControl:true,
+		overviewMapControl:true,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+
+	var contentInfo1 =
+	'<div class="info-content">'+
+	'<div class="pull-left">'+
+	'<img src="img/red-cross.png">'+
+	'</div>'+
+	'<div class="infowindow pull-right">'+
+	'<p><b>Bloodbank@HSA</b><br/>'+
+	'<br>Blood Services Group'+
+	'<br>Health Sciences Authority (opposite Outram Park MRT Station)'+
+	'<br>11 Outram Road'+
+	'<br>Singapore 169078'+
+	'</p>'+
+	'</div>'+
+	'</div>';
+
+	var contentInfo2 =
+	'<div class="info-content">'+
+	'<div class="pull-left">'+
+	'<img src="img/red-cross.png">'+
+	'</div>'+
+	'<div class="infowindow pull-right">'+
+	'<p><b>Bloodbank@Woodlands</b><br/>'+
+	'<br>900 South Woodlands Drive'+
+	'<br>#05-07 Woodlands Civic Centre (opposite Causeway Point)'+
+	'<br>Singapore 730900'+
+	'</p>'+
+	'</div>'+
+	'</div>';
+
+	var contentInfo3 =
+	'<div class="info-content">'+
+	'<div class="pull-left">'+
+	'<img src="img/red-cross.png">'+
+	'</div>'+
+	'<div class="infowindow pull-right">'+
+	'<p><b>Bloodbank@Dhoby Ghaut</b><br/>'+
+	'<br>Dhoby Xchange'+
+	'<br>11 Orchard Road'+
+	'<br>#B1-05 to 09'+
+	'<br>Singapore 238826'+
+	'</p>'+
+	'</div>'+
+	'</div>';
+
+	var setMarker = function (bank, map, contentInfo){
+	  var marker = new google.maps.Marker({
+	    position:bank,
+	    icon:'img/red-cross.png'
+	  });
+
+	  marker.setMap(map);
+	  markers.push(marker);
+
+	  marker.infowindow = new google.maps.InfoWindow({
+	    content: contentInfo,
+	    maxWidth: 256
+	  });
+
+	  google.maps.event.addListener(marker, 'click', function() {
+	    if (current_marker) {
+	      current_marker.infowindow.close();
+	    }
+	    current_marker = marker;
+	    marker.infowindow.open(map,marker);
+	  });
+	}
+
+	var initialize = function ()
+	{
+	  directionsDisplay = new google.maps.DirectionsRenderer();
+
+	  var elementmap = $('.for-mobile:visible').size() != 0 ? document.getElementById("mobilemap") : document.getElementById("desktopmap");
+
+	  map = new google.maps.Map(elementmap,mapProp);
+	  directionsDisplay.setMap(map);
+
+	  setMarker(new google.maps.LatLng(locations[0].lat, locations[0].lng), map, contentInfo1);
+	  setMarker(new google.maps.LatLng(locations[1].lat, locations[1].lng), map, contentInfo2);
+	  setMarker(new google.maps.LatLng(locations[2].lat, locations[2].lng), map, contentInfo3);
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
+
+	var zoomtoplace = function () 
+	{
+		if (current_marker) {
+	      current_marker.infowindow.close();
+	    }
+		var i = parseInt($('#bloodbanklist').val());
+		var selected_location = new google.maps.LatLng(locations[i].lat, locations[i].lng)
+		map.setCenter(selected_location);
+		current_marker = markers[i];
+  		markers[i].infowindow.open(map, markers[i]);
+
+	};
+
+	$('#bloodbanklist').on('change', function() {
+		zoomtoplace();
+	});
+
+	// methods for contact us form
+	// captcha generation
+	charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+	var generate = function(len) {
+		var output = '';
+		for(var i=0; i<len; i++) {
+		   output += charset[ Math.floor(Math.random()*charset.length) ];
+		}
+	    return output;
+	};
+
+	captcha = generate(5).toUpperCase();
+	$('.captcha').html(captcha);
+
+	$('.contactus').load( "contact.html", function() {
+		var contact_content = $('#contact-content').html();
+		$('.contactus').html('');
+
+		$(".contact-us").fancybox({
+			'content' : contact_content,
+			'transitionIn' : 'none',
+			'transitionOut' : 'none',
+			'type' : 'html',
+			afterShow: function(current, previous) {
+				console.log('form open');
+				captcha = generate(5).toUpperCase();
+
+				$('.captcha').html(captcha);
+
+				// Contact Form
+				$( '.form1' ).parsley();
+
+				$('.btn-reload').bind('click', function() {
+					captcha = generate(5).toUpperCase();
+					$('.captcha').html(captcha);
+				});
+
+				$("#contact-verify").focusout(function(){
+					var verify_code = $("#contact-verify").val();
+					if (verify_code.toUpperCase() == captcha ) {
+						$('#verify_fail').hide();
+						$('#contact-verify').removeClass('parsley-error').addClass('parsley-success');
+					} else {
+					  	$('#verify_fail').show();
+					  	$('#contact-verify').addClass('parsley-error').removeClass('parsley-success');
+					}
+				});
+
+				$('.btn-image').bind('click', function(){
+					
+					console.log('test');
+
+					$( '.form1' ).parsley( 'validate' );
+					var verify_code = $("#contact-verify").val();
+					
+					if (verify_code.toUpperCase() == captcha ) {
+					  var name = $("#contact-name").val();
+					  var email = $("#contact-email").val();
+					  var phone = $("#contact-phone").val();
+					  var subject = $("#contact-subject").val();
+					  var message= $("#contact-message").val();
+					  var dataString = 'name=' + name + '&email=' + email + '&phone=' + phone + '&subject=' + subject + '&message=' + message;
+
+					  if ($( '.form1' ).parsley( 'validate' )) {
+					    $.ajax({
+					      type: "POST",
+					      url: "app/app.php",
+					      data: dataString,
+					      success: function(data){
+					      	console.log(data);
+					      	if (data == 'result=0') {
+					      		$('.success').fadeIn(1000);
+					      		$( '.form1 input' ).val('');
+					      		$( '.form1 textarea' ).val('');
+					      	} else
+					      	{
+
+					      	}
+					      }
+					    });
+					    //$.fancybox.close();
+					  }
+					}
+					return false;
+				});
+			}
+		});
+	});
+
+	// where to donate - view all donation schdules
+	$(".view-all").fancybox({
+	   'width' : '75%',
+	   'height' : '75%',
+	   'autoScale' : false,
+	   'transitionIn' : 'none',
+	   'transitionOut' : 'none',
+	   'type' : 'iframe'
+	});
+
+	if ($('.for-mobile:visible').size() != 0)
+	{
+		$('.fb-facepile').attr('data-width', 300);
+	}
+
+	// blood stat
+	maxwidth = $('html').css('max-width');
+	if (maxwidth == '767px') {
+		var chartsize = 70;
+		var lineChartWidth = 8;
+	} else {
+		var chartsize = 110;
+		var lineChartWidth = 15;
+	}
+
+	var setbloodstat_text = function(bloodpercent) {
+		var text = '';
+		switch (bloodpercent) {
+			case '25' : 
+				text = 'very<br/>low';
+				break;
+			case '50' : 
+				text = 'low';
+				break;
+			case '75' : 
+				text = 'healthy';
+				break;
+		}
+		return text;
+	};
+
+	$.ajax({
+	    type:"GET",
+	    url: "app/app.php?method=bloodstat",
+	    success: function(data){
+	    	var bloodstat = data.split('-');
+
+	    	// hard code for now until the php to google spreadsheet is fixed
+	    	bloodstat[0] = 50;
+	    	bloodstat[1] = 50;
+	    	bloodstat[2] = 75;
+	    	bloodstat[3] = 25;
+
+	        $('.blood-a .chart').attr('data-percent', bloodstat[0]);
+	        $('.blood-b .chart').attr('data-percent', bloodstat[1]);
+	        $('.blood-o .chart').attr('data-percent', bloodstat[2]);
+	        $('.blood-ab .chart').attr('data-percent', bloodstat[3]);
+
+	        $('.blood-a .status label').html( setbloodstat_text(bloodstat[0]) );
+	        $('.blood-b .status label').html( setbloodstat_text(bloodstat[1]) );
+	        $('.blood-o .status label').html( setbloodstat_text(bloodstat[2]) );
+	        $('.blood-ab .status label').html( setbloodstat_text(bloodstat[3]) );
+
+			$('.chart').easyPieChart({
+				animate: 2000,
+				lineWidth: lineChartWidth,
+				barColor: '#c1272c',
+				trackColor:'#e1e1e1',
+				size: chartsize ,
+			});
+
+
+	    }
+	});
+
+});
